7e4594ad197282c402e028d568acfc786a24917c
sarim <sarimk80>
2019-06-19 11:25:43 +0500

    Converting rxjava single to livedata with the help of livedata reactive stream library

--- app/src/main/java/com/example/openweather/Weather.java
+++ app/src/main/java/com/example/openweather/Weather.java
@@ -13,6 +13,8 @@ import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import androidx.lifecycle.ViewModelProviders;
+
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,19 +23,16 @@ import android.view.ViewGroup;
 
 import com.bumptech.glide.Glide;
 import com.example.openweather.databinding.FragmentWeatherBinding;
-import com.example.openweather.model.WeatherModel;
-import com.example.openweather.remote.APIService;
-import com.example.openweather.remote.RetroClass;
+
+import com.example.openweather.view_model.WeatherViewModel;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 
 
 import java.util.Objects;
 
-import io.reactivex.Single;
-import io.reactivex.SingleObserver;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
+
+
+
 
 
 /**
@@ -41,7 +40,7 @@ import io.reactivex.schedulers.Schedulers;
  */
 public class Weather extends Fragment {
 
-    private Disposable disposable;
+
     private FragmentWeatherBinding weatherBinding;
     private RxPermissions rxPermissions;
 
@@ -72,9 +71,9 @@ public class Weather extends Fragment {
                 .subscribe(granted -> {
                     if (granted) {
                         getData();
-                        Log.d("Main", "Permission is granted");
+                        //Log.d("Main", "Permission is granted");
                     } else {
-                        Log.d("Main", "Permission is not granted");
+                        //Log.d("Main", "Permission is not granted");
                     }
                 });
 
@@ -83,7 +82,7 @@ public class Weather extends Fragment {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        disposable.dispose();
+
     }
 
     private void getData() {
@@ -100,33 +99,42 @@ public class Weather extends Fragment {
 
         //Getting the Location End
 
-        //Getting the data from Retrofit
-        APIService apiService = RetroClass.getApiService();
+        WeatherViewModel weatherViewModel = ViewModelProviders.of(Weather.this).get(WeatherViewModel.class);
 
-        Single<WeatherModel> modelSingle = apiService.getWeatherModel(Latitude, Longitude, "b6907d289e10d714a6e88b30761fae22")
-                .subscribeOn(Schedulers.io())//Will run on background
-                .observeOn(AndroidSchedulers.mainThread());//Will run on main thread
-
-        modelSingle.subscribe(new SingleObserver<WeatherModel>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                disposable = d;
-            }
-
-            @Override
-            public void onSuccess(WeatherModel weatherModel) {
-//                Log.d("Weather", String.valueOf(weatherModel.getMain().getTemp()));
-                weatherBinding.setWeather(weatherModel);
-                Glide.with(Objects.requireNonNull(getActivity())).load("http://openweathermap.org/img/w/" + weatherModel.getWeather().get(0).getIcon() + ".png").into(weatherBinding.image);
+        weatherViewModel.getViewModel(Latitude, Longitude, "b6907d289e10d714a6e88b30761fae22").observe(Weather.this, weatherModel -> {
+            weatherBinding.setWeather(weatherModel);
+            Glide.with(Objects.requireNonNull(getActivity())).load("http://openweathermap.org/img/w/" + weatherModel.getWeather().get(0).getIcon() + ".png").into(weatherBinding.image);
 
                 weatherBinding.progress.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                weatherBinding.progress.setVisibility(View.GONE);
-            }
         });
+
+        //Getting the data from Retrofit
+//        APIService apiService = RetroClass.getApiService();
+//
+//        Single<WeatherModel> modelSingle = apiService.getWeatherModel(Latitude, Longitude, "b6907d289e10d714a6e88b30761fae22")
+//                .subscribeOn(Schedulers.io())//Will run on background
+//                .observeOn(AndroidSchedulers.mainThread());//Will run on main thread
+//
+//        modelSingle.subscribe(new SingleObserver<WeatherModel>() {
+//            @Override
+//            public void onSubscribe(Disposable d) {
+//                disposable = d;
+//            }
+//
+//            @Override
+//            public void onSuccess(WeatherModel weatherModel) {
+////                Log.d("Weather", String.valueOf(weatherModel.getMain().getTemp()));
+//                weatherBinding.setWeather(weatherModel);
+//                Glide.with(Objects.requireNonNull(getActivity())).load("http://openweathermap.org/img/w/" + weatherModel.getWeather().get(0).getIcon() + ".png").into(weatherBinding.image);
+//
+//                weatherBinding.progress.setVisibility(View.GONE);
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//                weatherBinding.progress.setVisibility(View.GONE);
+//            }
+//        });
         //Getting the data from Retrofit
     }
 }
--- app/src/main/java/com/example/openweather/remote/APIService.java
+++ app/src/main/java/com/example/openweather/remote/APIService.java
@@ -2,12 +2,14 @@ package com.example.openweather.remote;
 
 import com.example.openweather.model.WeatherModel;
 
+import io.reactivex.Flowable;
 import io.reactivex.Single;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface APIService {
 
+    //TO CONVERT RXJAVA TO LIVEDATA WE CAN ONLY USE FLOWABLE OR FLATMAPSINGLE
     @GET("weather")
-    Single<WeatherModel> getWeatherModel(@Query("lat") String lat, @Query("lon") String lon, @Query("appid") String appid);
+    Flowable<WeatherModel> getWeatherModel(@Query("lat") String lat, @Query("lon") String lon, @Query("appid") String appid);
 }
